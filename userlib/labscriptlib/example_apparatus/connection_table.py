919dd486e6e73e8e3b26b0991f095f31f28951b1
Shafin <haqueshafinul@gmail.com>
2024-07-16 13:47:27 -0400

    user pineblaster, modify connection table

--- userlib/labscriptlib/example_apparatus/connection_table.py
+++ userlib/labscriptlib/example_apparatus/connection_table.py
@@ -1,19 +1,22 @@
 from labscript import *
 from labscript_devices.PulseBlasterUSB import PulseBlasterUSB
+from labscript_devices.PineBlaster import PineBlaster
 from labscript_devices.NI_DAQmx.labscript_devices import NI_PCIe_6363
 from labscript_devices.FunctionRunner.labscript_devices import FunctionRunner
 
 '''
 Initialize the FunctionRunner that runs scripts before and after shots
 '''
-FunctionRunner(name='feedback')
+FunctionRunner(name='feedback1')
 
 '''
 Initialize the PulseBlaster as the Pseudoclock
 and all the channels to be used
 '''
-PulseBlasterUSB(name='pb',board_number=0,programming_scheme='pb_start/BRANCH')
-ClockLine(name='pb_clockline_fast', pseudoclock=pb.pseudoclock,connection='flag 0')
+# PulseBlasterUSB(name='pb',board_number=0,programming_scheme='pb_start/BRANCH')
+# ClockLine(name='pb_clockline_fast', pseudoclock=pb.pseudoclock,connection='flag 0')
+PineBlaster(name='pb', usbport='COM3')
+# ClockLine(name='pb_clockline_fast', pseudoclock=pb.pseudoclock,connection='flag 0')
 
 '''
 Initialize the NI Hardware and all the channels
@@ -23,8 +26,8 @@ ni_6363_max_name = "ni_6363"
 
 NI_PCIe_6363(
     name='ni_6363', 
-    parent_device=pb_clockline_fast,
-    clock_terminal=f'/{ni_6363_max_name}/PFI0',
+    parent_device=pb_clock_line,
+    clock_terminal=f'/{ni_6363_max_name}/PFI1',
     MAX_name=f'{ni_6363_max_name}',
     acquisition_rate=1000e3,
     stop_order=-1,
