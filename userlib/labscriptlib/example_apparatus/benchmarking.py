63e055837c6fc1f2b51613392be35851cf287a42
Shafin <haqueshafinul@gmail.com>
2024-08-05 09:20:38 -0400

    update connection table based on new hardware setup

--- userlib/labscriptlib/example_apparatus/benchmarking.py
+++ userlib/labscriptlib/example_apparatus/benchmarking.py
@@ -12,66 +12,83 @@ and all the channels to be used
 # PulseBlasterUSB(name='pb',board_number=0,programming_scheme='pb_start/BRANCH')
 # ClockLine(name='pb_clock_line', pseudoclock=pb.pseudoclock,connection='flag 0')
 # PineBlaster(name='pb', usbport='COM3')
-PrawnBlaster(name='pb',com_port='COM5')
+PrawnBlaster(
+    name='pb',
+    com_port='COM5',
+    num_pseudoclocks=2
+)
 
 '''
 Initialize the NI Hardware and all the channels
 to be used on each card
 '''
-ni_6363_max_name = "ni_6363"
-NI_PCIe_6363(
-    name='ni_6363_mio', 
-    parent_device=pb_clock_line_0,
-    clock_terminal=f'/{ni_6363_max_name}/PFI1',
-    MAX_name=f'{ni_6363_max_name}',
+ni_6361_max_name = "PXI1Slot8"
+NI_PXIe_6361(
+    name='ni_6361_mio', 
+    parent_device=pb_clock_line_1,
+    clock_terminal=f'/{ni_6361_max_name}/PFI1',
+    MAX_name=f'{ni_6361_max_name}',
     acquisition_rate=250e3,
 )
-AnalogOut(name='ao0_6363', parent_device=ni_6363_mio, connection='ao0')
-AnalogOut(name='ao1_6363', parent_device=ni_6363_mio, connection='ao1')
-DigitalOut(name='do0_6363', parent_device=ni_6363_mio, connection='port0/line0')
-DigitalOut(name='do1_6363', parent_device=ni_6363_mio, connection='port0/line1')
-AnalogIn(name='ai0_6363', parent_device=ni_6363_mio, connection='ai0')
-AnalogIn(name='ai1_6363', parent_device=ni_6363_mio, connection='ai1')
+AnalogOut(name='ao0_6361', parent_device=ni_6361_mio, connection='ao0')
+AnalogOut(name='ao1_6361', parent_device=ni_6361_mio, connection='ao1')
+DigitalOut(name='do0_6361', parent_device=ni_6361_mio, connection='port0/line0')
+DigitalOut(name='do1_6361', parent_device=ni_6361_mio, connection='port0/line1')
+AnalogIn(name='ai0_6361', parent_device=ni_6361_mio, connection='ai0')
+AnalogIn(name='ai1_6361', parent_device=ni_6361_mio, connection='ai1')
+
+
+ni_6739_max_name = "PXI1Slot3"
+NI_PXIe_6739(
+    name='ni_6739_ao', 
+    parent_device=pb_clock_line_0,
+    clock_terminal=f'/{ni_6739_max_name}/PFI0',
+    MAX_name=f'{ni_6739_max_name}',
+)
+AnalogOut(name='ao0_6739', parent_device=ni_6739_ao, connection='ao0')
+AnalogOut(name='ao1_6739', parent_device=ni_6739_ao, connection='ao4')
+AnalogOut(name='ao2_6739', parent_device=ni_6739_ao, connection='ao8')
+AnalogOut(name='ao3_6739', parent_device=ni_6739_ao, connection='ao12')
 
 '''
 Define the Experiment Logic
 '''
 def digital_start_ref():
     t=0
-    do1_6363.go_high(t)
+    do1_6361.go_high(t)
     t+=10e-3
-    do1_6363.go_low(t)
+    do1_6361.go_low(t)
     return t
 
 def digital_pwm_stream():
     t=ctrl_pwm_ti
     # add_time_marker(t, "D0 PWM CTRL")
     while t<=ctrl_pwm_tf:
-        do0_6363.go_high(t)
+        do0_6361.go_high(t)
         t+=((1/ctrl_pwm_freq) * ctrl_duty_cycle)
-        do0_6363.go_low(t)
+        do0_6361.go_low(t)
         t+=((1/ctrl_pwm_freq) * (1-ctrl_duty_cycle))
     return t
 
 def analog_output_ramp():
     t=ramp_ti
-    ao0_6363.constant(t=0, value=0)
+    ao0_6361.constant(t=0, value=0)
     # add_time_marker(t, "A0 Ramp", verbose=True)
-    t+=ao0_6363.ramp(
+    t+=ao0_6361.ramp(
         t=t, 
         initial=ramp_vi, 
         final=ramp_vf, 
         duration=ramp_dur, 
         samplerate=ramp_rate
     )
-    ao0_6363.constant(t=t, value=(ramp_vf/2))
+    ao0_6361.constant(t=t, value=(ramp_vf/2))
     return t
 
 def analog_output_exp_ramp():
     t=0
-    ao1_6363.constant(t=t, value=exp_ramp_vi)
+    ao1_6361.constant(t=t, value=exp_ramp_vi)
     t=exp_ramp_ti
-    t+=ao1_6363.sine4_reverse_ramp(
+    t+=ao1_6361.sine4_reverse_ramp(
         t=t,
         initial=exp_ramp_vf,
         final=exp_ramp_vi,
@@ -79,7 +96,7 @@ def analog_output_exp_ramp():
         samplerate=exp_ramp_rate,
         truncation=0.8
     )
-    t+=ao1_6363.sine_ramp(
+    t+=ao1_6361.sine_ramp(
         t=t,
         initial=exp_ramp_vf,
         final=exp_ramp_vi,
@@ -90,12 +107,12 @@ def analog_output_exp_ramp():
 
 def collect_AO_0():
     t=ramp_ti
-    t+=ai0_6363.acquire(label='AO_ramp', start_time=t, end_time=exp_ramp_tf)
+    t+=ai0_6361.acquire(label='AO_ramp', start_time=t, end_time=exp_ramp_tf)
     return t
 
 def collect_AO_1():
     t=exp_ramp_ti
-    t+=ai1_6363.acquire(label='AI_signal', start_time=0, end_time=exp_ramp_tf)
+    t+=ai1_6361.acquire(label='AI_signal', start_time=0, end_time=exp_ramp_tf)
     return t
 
 def collect_dummy_inputs():
--- userlib/labscriptlib/example_apparatus/connection_table.py
+++ userlib/labscriptlib/example_apparatus/connection_table.py
@@ -12,32 +12,20 @@ and all the channels to be used
 # PulseBlasterUSB(name='pb',board_number=0,programming_scheme='pb_start/BRANCH')
 # ClockLine(name='pb_clock_line', pseudoclock=pb.pseudoclock,connection='flag 0')
 # PineBlaster(name='pb', usbport='COM3')
-PrawnBlaster(name='pb',com_port='COM5')
+PrawnBlaster(
+    name='pb',
+    com_port='COM5',
+    num_pseudoclocks=2
+)
 
 '''
 Initialize the NI Hardware and all the channels
 to be used on each card
 '''
-ni_6363_max_name = "ni_6363"
-NI_PCIe_6363(
-    name='ni_6363_mio', 
-    parent_device=pb_clock_line_0,
-    clock_terminal=f'/{ni_6363_max_name}/PFI1',
-    MAX_name=f'{ni_6363_max_name}',
-    acquisition_rate=1000e3,
-)
-AnalogOut(name='ao0_6363', parent_device=ni_6363_mio, connection='ao0')
-AnalogOut(name='ao1_6363', parent_device=ni_6363_mio, connection='ao1')
-DigitalOut(name='do0_6363', parent_device=ni_6363_mio, connection='port0/line0')
-DigitalOut(name='do1_6363', parent_device=ni_6363_mio, connection='port0/line1')
-AnalogIn(name='ai0_6363', parent_device=ni_6363_mio, connection='ai0')
-AnalogIn(name='ai1_6363', parent_device=ni_6363_mio, connection='ai1')
-
-
 ni_6361_max_name = "PXI1Slot8"
 NI_PXIe_6361(
     name='ni_6361_mio', 
-    parent_device=pb_clock_line_0,
+    parent_device=pb_clock_line_1,
     clock_terminal=f'/{ni_6361_max_name}/PFI1',
     MAX_name=f'{ni_6361_max_name}',
     acquisition_rate=1000e3,
@@ -62,11 +50,12 @@ DigitalOut(name='do1_6535', parent_device=ni_6365_dio, connection='port0/line1')
 DigitalOut(name='do2_6535', parent_device=ni_6365_dio, connection='port0/line2')
 DigitalOut(name='do3_6535', parent_device=ni_6365_dio, connection='port0/line3')
 
+
 ni_6739_max_name = "PXI1Slot3"
 NI_PXIe_6739(
     name='ni_6739_ao', 
     parent_device=pb_clock_line_0,
-    clock_terminal=f'/{ni_6739_max_name}/PFI1',
+    clock_terminal=f'/{ni_6739_max_name}/PFI0',
     MAX_name=f'{ni_6739_max_name}',
 )
 AnalogOut(name='ao0_6739', parent_device=ni_6739_ao, connection='ao0')
